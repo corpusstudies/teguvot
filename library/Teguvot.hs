b18e1092188e91a393f5647abf54a839c8610e67
Scott Fleischman <scott-fleischman@users.noreply.github.com>
2021-10-24 08:13:19 -0700

    Check missing analyses.

--- library/Teguvot.hs
+++ library/Teguvot.hs
@@ -9,6 +9,7 @@ import Data.Foldable (foldl')
 import Text.Pretty.Simple (pPrintLightBg)
 import GHC.Generics (Generic)
 import Control.Lens ((^.))
+import Data.Maybe (mapMaybe)
 
 data DupCategoryItem = DupCategoryItem
   { name :: AnalysisName
@@ -16,6 +17,21 @@ data DupCategoryItem = DupCategoryItem
   , existing :: Location
   }
   deriving (Generic, Show)
+
+checkForMissingAnalyses :: Map AnalysisName CategoryItem
+  -> [Analysis] -> IO ()
+checkForMissingAnalyses categoryMap analyses = do
+  let check analysis@Analysis{name} =
+        case Map.lookup name categoryMap of
+          Nothing -> Just analysis
+          Just _ -> Nothing
+  let missing = mapMaybe check analyses
+  case missing of
+    [] -> pure ()
+    _ : _ -> do
+      putStrLn "Error: missing analyses"
+      mapM_ pPrintLightBg missing
+      exitFailure
 
 loadCategoryMap :: FilePath ->
   IO (Map AnalysisName CategoryItem)
@@ -35,7 +51,7 @@ loadCategoryMap filePath = do
                 existingMap
             )
           Just existingItem ->
-            ( DupCategoryItem 
+            ( DupCategoryItem
                 { name = analysisName
                 , dup =
                     newItem ^. #analysis . #location
@@ -48,7 +64,7 @@ loadCategoryMap filePath = do
 
     dupCategories :: [DupCategoryItem]
     categoryMap :: Map AnalysisName CategoryItem
-    (dupCategories, categoryMap) = 
+    (dupCategories, categoryMap) =
       foldl'
         go
         (mempty, mempty)
@@ -59,13 +75,29 @@ loadCategoryMap filePath = do
       putStrLn "Error: duplicate categories"
       mapM_ pPrintLightBg dupCategories
       exitFailure
+
+  let referencedAnalyses =
+        concatMap
+          (^. #implications)
+          categoryMap
+  checkForMissingAnalyses categoryMap referencedAnalyses
+
   pure categoryMap
+
+getAnalyses :: AnalysisItem -> [Analysis]
+getAnalyses = \case
+  AnalysisItemWord CorpusWord {analyses} -> analyses
+  AnalysisItemCombo Combo {analyses} -> analyses
 
 main :: IO ()
 main = do
+  categoryMap <- loadCategoryMap "data/category.txt"
+
   analysisItems <-
     readParseAnalysisFile "data/analysis.txt"
-  categoryMap <- loadCategoryMap "data/category.txt"
+  checkForMissingAnalyses
+    categoryMap
+    (concatMap getAnalyses analysisItems)
 
   putStrLn $
     (show . length) analysisItems
